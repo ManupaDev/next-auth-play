d6bb266ffea4744565bfc89eea2567a5d39ea787
ManupaDev <manupadevsuite@gmail.com>
2023-01-17 09:34:28 +0530

    cosmetic edits

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head />
-      <body className="bg-[#1e1e1e] text-white">{children}</body>
+      <body className="bg-[#1e1e1e] text-white">
+        {children}
+      </body>
     </html>
   );
 }
